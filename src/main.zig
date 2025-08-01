65e56b611ae280465632c202ff3e6ac398337e54
Vojtěch Tilhon <wojtat@email.cz>
2025-08-01 10:16:51 +0200

    Implement a simple command line interface
    The program now accepts an arbitrary amount of command line arguments.
    Each argument is a relative file path to a file containing lisp
    expressions. The expressions from all the files combined are then
    executed in the same environment and in the given order.

--- src/main.zig
+++ src/main.zig
@@ -3,6 +3,34 @@ const Allocator = std.mem.Allocator;
 const Parser = @import("Parser.zig");
 const Evaluator = @import("Evaluator.zig");
 
+fn readEntireFile(allocator: Allocator, relative_path: [:0]const u8) ![]u8 {
+    var file = try std.fs.cwd().openFile(relative_path, .{});
+    defer file.close();
+    const file_size = try file.getEndPos();
+    const buffer = try allocator.alloc(u8, file_size);
+    errdefer allocator.free(buffer);
+    _ = try file.read(buffer);
+    return buffer;
+}
+
+fn parseAndEvaluateAll(allocator: Allocator, evaluator: *Evaluator, source: []const u8) !void {
+    var parser = Parser.init(allocator, source);
+    while (try parser.parseExpression()) |expression| {
+        defer expression.deinit(allocator);
+        // TODO: Write to stdout, not stderr
+        std.debug.print("Expression: ", .{});
+        expression.debugPrint();
+        std.debug.print("\n", .{});
+
+        const evaluated = try evaluator.evaluate(expression);
+        defer evaluated.deinit(allocator);
+        // TODO: Write to stdout, not stderr
+        std.debug.print("Evaluates to: ", .{});
+        evaluated.debugPrint();
+        std.debug.print("\n", .{});
+    }
+}
+
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
@@ -11,33 +39,15 @@ pub fn main() !void {
         _ = status;
     }
 
-    const source =
-        \\(define factorial (lambda (n)
-        \\    (if (zero? n)
-        \\        1
-        \\        (* n (factorial (- n 1))))))
-        \\
-        \\(define fact factorial)
-        \\
-        \\(define f10 (fact 10))
-        \\
-        \\f10
-    ;
-
-    var parser = Parser.init(allocator, source);
     var evaluator = try Evaluator.init(allocator);
     defer evaluator.deinit();
 
-    while (try parser.parseExpression()) |expression| {
-        defer expression.deinit(allocator);
-        std.debug.print("Expression: ", .{});
-        expression.debugPrint();
-        std.debug.print("\n", .{});
+    var args_iter = std.process.args();
+    _ = args_iter.skip();
+    while (args_iter.next()) |arg| {
+        const contents = try readEntireFile(allocator, arg);
+        defer allocator.free(contents);
 
-        const evaluated = try evaluator.evaluate(expression);
-        defer evaluated.deinit(allocator);
-        std.debug.print("Evaluates to: ", .{});
-        evaluated.debugPrint();
-        std.debug.print("\n", .{});
+        try parseAndEvaluateAll(allocator, &evaluator, contents);
     }
 }
