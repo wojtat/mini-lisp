3c12c246cff5068c6e01149311710e667b954079
Vojtěch Tilhon <wojtat@email.cz>
2025-08-03 14:39:45 +0200

    Rename project to mini-lisp

--- build.zig
+++ build.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const exe = b.addExecutable(.{
-        .name = "lisp-zig",
+        .name = "mini-lisp",
         .root_source_file = b.path("src/main.zig"),
         .target = b.standardTargetOptions(.{}),
         .optimize = b.standardOptimizeOption(.{}),
